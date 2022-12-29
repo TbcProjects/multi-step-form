c79c8e1fdd440d7951d66d86c0534d3e5b7782b0
TbcProjects <daniel@thebikeclub.co.uk>
2022-12-29 16:06:05 +0000

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import appTheme from "./theme";
+
+import { ChakraProvider } from "@chakra-ui/react";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <ChakraProvider theme={appTheme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
---
+++ src/theme/index.ts
@@ -0,0 +1,5 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const appTheme = extendTheme({});
+
+export default appTheme;
